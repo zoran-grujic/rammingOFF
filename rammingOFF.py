872bc8ef4fb08e13946936db31f2f9f3e93129ea
Zoran Grujic <zoran.grujic@gmail.com>
2025-11-08 22:20:28 +0100

    Add licence info

--- rammingOFF.py
+++ rammingOFF.py
@@ -1,3 +1,39 @@
+#!/usr/bin/env python3
+"""
+rammingOFF - OrcaSlicer Multi-Material Post-Processing Script
+
+Optimizes multi-color/multi-material gcode for single-extruder 3D printers
+with manual filament changes. Removes wasteful ramming and priming sequences,
+adds Z-compensation, and manages temperatures during toolchanges.
+
+Author: Zoran D. Grujić
+GitHub: https://github.com/zoran-grujic
+Repository: https://github.com/zoran-grujic/rammingOFF
+License: MIT License
+Version: 2.0
+Date: November 2025
+
+Copyright (c) 2025 Zoran D. Grujić
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import sys
 import os
 import re
